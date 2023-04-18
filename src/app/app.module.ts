52ad6675e6e65f623fdb9dbf06648190d54fc50b
Hanzhang ma <583886027@qq.com>
2023-04-18 21:58:20 +0200

    137 add ? & to the router
    use [routerLink]="[]"
    [queryParams]="{key: value}" to indicate ?
    [fragment]="" to indicate #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
   {path: 'users/:id/:name', component: UserComponent},
+  {path: 'servers/:id/edit', component: EditServerComponent},
   {path: 'servers', component: ServersComponent}
 ];
 @NgModule({
