cfa7db6bb7aea2807fe00c7a7fdbad5e18f32fca
Hanzhang ma <583886027@qq.com>
2023-04-28 10:48:41 +0200

    151 pass static route data
    we want to pass a parameter from the route to the component.
    
    1. fill the data field in the route array
    
    2. initialize an ActivatedRoute instance in constructor.
    
    3. subscribe the data from the route by this.route.data.subscribe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import {ServerComponent} from "./servers/server/server.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AuthGuard} from "./auth-guard.service";
 import {CanDeactivateGuard} from "./servers/edit-server/can-activate-guard.service";
+import {ErrorPageComponent} from "./error-page/error-page.component";
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -19,7 +20,7 @@ const appRoutes: Routes = [
       {path: ':id/edit', component: EditServerComponent, canDeactivate:[CanDeactivateGuard]},
       {path: ':id', component: ServerComponent},
     ]},
-  {path: 'not-found', component: PageNotFoundComponent},
+  {path: 'not-found', component: ErrorPageComponent,data:{message: 'Page not found!'}},
   {path: '**', redirectTo: '/not-found'}
 ];
 @NgModule({
