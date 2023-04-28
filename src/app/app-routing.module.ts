13f235615ab410b89c57168f07bf65082af28c6b
Hanzhang ma <583886027@qq.com>
2023-04-28 14:42:26 +0200

    152 Resolve dynamical data with the Resolve Guard.
    We want to run some code before rendering. We also want to determine which code could be rendered. So we use a resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AuthGuard} from "./auth-guard.service";
 import {CanDeactivateGuard} from "./servers/edit-server/can-activate-guard.service";
 import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerResolver} from "./servers/server/server-resolver.service.js";
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -17,8 +18,8 @@ const appRoutes: Routes = [
       {path: ':id/:name', component: UserComponent},
     ]},
   {path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent,children: [
+      {path: ':id', component: ServerComponent, resolve: {server: ServerResolver}},
       {path: ':id/edit', component: EditServerComponent, canDeactivate:[CanDeactivateGuard]},
-      {path: ':id', component: ServerComponent},
     ]},
   {path: 'not-found', component: ErrorPageComponent,data:{message: 'Page not found!'}},
   {path: '**', redirectTo: '/not-found'}
